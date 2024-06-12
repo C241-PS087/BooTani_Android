fef5ecb1e9ff98178eafd45f6b03a30425cd065e
Nathaniell33 <onevaldosihotang33@gmail.com>
2024-06-12 10:49:29 +0700

    Add Artikel by Valdo

---
+++ app/src/main/java/com/example/bootani/DetailActivity.kt
@@ -0,0 +1,35 @@
+package com.example.bootani
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var tvName: TextView
+    private lateinit var tvDescription: TextView
+    private lateinit var ivPhoto: ImageView
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        tvName = findViewById(R.id.tv_item_name)
+        tvDescription = findViewById(R.id.tv_item_description)
+        ivPhoto = findViewById(R.id.img_item_photo)
+
+        val dataArtikel = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra<Artikel>("key_artikel", Artikel::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra<Artikel>("key_artikel")
+        }
+
+        tvName.text = dataArtikel?.name
+        tvDescription.text = dataArtikel?.description
+        ivPhoto.setImageResource(dataArtikel?.photo ?: R.drawable.padi)
+    }
+}
